41b933548bd3bc44ab307b3e9153e15c08100a1e
ayeshaf19m01 <136219927+ayeshaf19m01@users.noreply.github.com>
2025-05-14 15:24:48 +0500

    socket file change

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,8 +1,11 @@
 // frontend/utils/socket.js
 import { io } from 'socket.io-client';
-
+// frontend/utils/socket.js
 const socket = io('https://wedbricksbackend-production.up.railway.app', {
-  transports: ['websocket'], // optional but recommended
-});
+    transports: ['websocket'],
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 3000,
+  });
 
 export default socket;
